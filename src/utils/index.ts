461785fa9a98d15aaea3acd922cc7c8ca7e81d6c
foxmon <jjockrod@naver.com>
2023-04-12 23:20:24 +0900

    util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,13 @@
-import { isArray, isString, isNumber, isBoolean } from "./is";
-import { assertString, assertArray } from "./assert";
+import { isArray, isString, isNumber, isBoolean, isFunction } from "./is";
+import { assertString, assertArray, assertFunction } from "./assert";
 
-export { isArray, isString, isNumber, isBoolean, assertString, assertArray };
+export {
+  isArray,
+  isString,
+  isNumber,
+  isBoolean,
+  isFunction,
+  assertString,
+  assertArray,
+  assertFunction,
+};
