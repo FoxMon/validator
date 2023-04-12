f0fb2e699a2fa77938669b708bc2e075bafcbd30
foxmon <jjockrod@naver.com>
2023-04-12 23:21:06 +0900

    isEmpty

--- lib/common/isEmpty.ts
+++ lib/common/isEmpty.ts
@@ -1,12 +1,28 @@
-import { CommonType } from "../../src/types";
+import { CommonType, DecoratorType } from "../../src/types";
+import { registDecorator } from "../core";
+
+const DECORATOR_NAME: string = "IS_EMPTY";
 
 /**
  * Check target is empty
  * @param {CommonType.CommonVariable} target
  * @returns {boolean}
  */
-export function isEmpty<T extends CommonType.CommonVariable>(
-  target: T
-): boolean {
+function _isEmpty<T extends CommonType.CommonVariable>(target: T): boolean {
   return target === "" || target === null || target === undefined;
+}
+
+/**
+ * Regist isEmpty decorator
+ * @param {DecoratorType.DecoratorOptions} decoratorOptions
+ */
+export function isEmpty(decoratorOptions?: DecoratorType.DecoratorOptions) {
+  // Regist decorator
+  registDecorator(
+    {
+      decoratorName: DECORATOR_NAME,
+      func: _isEmpty,
+    },
+    decoratorOptions
+  );
 }
