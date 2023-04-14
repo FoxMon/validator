f6115e832abab22557f495d4f7a95cfb7ed1ec80
foxmon <jjockrod@naver.com>
2023-04-14 23:27:09 +0900

    isNumber

---
+++ lib/common/isNumber.ts
@@ -0,0 +1,36 @@
+import { CommonType, DecoratorType } from "../../src/types";
+import { isNumber as isNumberCheck } from "../../src/utils";
+import { applyDecorator } from "../core";
+
+const DECORATOR_NAME: string = "IS_NUMBER";
+const DECORATOR_APPLY_TYPE: string = "property";
+
+/**
+ * Check target is number
+ * @param {CommonType.CommonVariable} target
+ * @returns {boolean}
+ */
+function _isNumber<T extends CommonType.CommonVariable>(target: T): boolean {
+  if (isNumberCheck(target)) {
+    return true;
+  }
+  return false;
+}
+
+/**
+ * Regist isNumber decorator
+ * @param {DecoratorType.DecoratorOptions} decoratorOptions
+ * @returns {Function}
+ */
+export function isNumber(
+  decoratorOptions?: DecoratorType.DecoratorOptions
+): Function {
+  return applyDecorator(
+    {
+      decoratorName: DECORATOR_NAME,
+      applyType: DECORATOR_APPLY_TYPE,
+      decoratorFunc: _isNumber,
+    },
+    decoratorOptions
+  );
+}
