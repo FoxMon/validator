0dbb255db84b8afaa73049cf799af56fdf8c1542
foxmon <jjockrod@naver.com>
2023-04-15 21:05:29 +0900

    lib

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,3 @@
-import { isEmpty, isNumber, isString } from "./common";
+import { isEmpty, isNumber, isString, isContain } from "./common";
 
-export { isEmpty, isNumber, isString };
+export { isEmpty, isNumber, isString, isContain };
