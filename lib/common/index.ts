df9e65d93b0023a7d2e7e289fed139ed2e724394
foxmon <jjockrod@naver.com>
2023-04-20 21:59:03 +0900

    common

--- lib/common/index.ts
+++ lib/common/index.ts
@@ -3,5 +3,6 @@ import { isNumber } from "./isNumber";
 import { isString } from "./isString";
 import { isContain } from "./isContain";
 import { isBoolean } from "./isBoolean";
+import { isArray } from "./isArray";
 
-export { isEmpty, isNumber, isString, isContain, isBoolean };
+export { isEmpty, isNumber, isString, isContain, isBoolean, isArray };
